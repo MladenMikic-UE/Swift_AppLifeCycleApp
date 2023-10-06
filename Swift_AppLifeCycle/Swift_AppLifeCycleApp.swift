463d49ccad0f76d79bcb216563c8d130121068c4
Mladen Mikic <m.mikic@rooom.com>
2023-10-06 17:04:34 +0200

    Initial Commit

---
+++ Swift_AppLifeCycle/Swift_AppLifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AppLifeCycleApp.swift
+//  Swift_AppLifeCycle
+//
+//  Created by MladenMikic# on 06.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct Swift_AppLifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
